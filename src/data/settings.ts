807a3a15cd38fbc5bfdde9afde880e70ac6794e6
IntrepidShape <dev@intrepidshape.com>
2023-04-28 21:38:51 +0800

    canonical url

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
-  site: 'https://your-domain.com',
+  site: 'https://gainsmultiplier.vercel.app/',
   name: 'Multiplier of Gains Calculator',
   title: 'Multiplier of Gains Calculator | A simple tool to calculate your investment growth',
   description: 'This is a Multiplier of Gains Calculator built with Astro, allowing users to calculate how many times their investments have increased in value.',
